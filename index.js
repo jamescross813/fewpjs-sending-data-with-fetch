1f55c274eb71f1de7bb95038827f5fb3cef15197
jamescross <crossjames813@gmail.com>
2021-08-11 21:54:37 -0500

    commit tree de534baf4b6e91f9afd99545d8ae8f4e4a902699

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+
+
+function submitData(name, email){
+    let formData = {
+        name: name,
+        email: email
+    };
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+    return fetch("http://localhost:3000/users", configObj)
+    .then(resp => resp.json())
+    .then(function(object){
+        let idElement = document.createElement('id');
+        idElement.innerHTML = object.id;
+        document.body.appendChild(idElement);
+    })
+    .catch(function(error){
+       
+        let errorMessage = document.createElement('h3');
+        errorMessage.innerHTML = error.message;
+        document.body.appendChild(errorMessage);
+        console.log(error.message);
+        
+    })
+}
